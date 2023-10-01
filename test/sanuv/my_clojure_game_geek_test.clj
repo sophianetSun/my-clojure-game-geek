fd412bec135f13bbe6d3c4cb557e92739f99e027
sophianetsun <sophianet.sun@gmail.com>
2023-10-01 23:21:28 +0900

    lacinia tutorial init

---
+++ test/sanuv/my_clojure_game_geek_test.clj
@@ -0,0 +1,7 @@
+(ns sanuv.my-clojure-game-geek-test
+  (:require [clojure.test :refer :all]
+            [sanuv.my-clojure-game-geek :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
